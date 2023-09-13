5117bff1622c09e2aa2033a439cc42e36c119701
oliverater <tomoliverprater@gmail.com>
2023-09-13 22:59:59 +0100

    integrate tooltip into app

--- src/App.svelte
+++ src/App.svelte
@@ -88,20 +88,16 @@ const impact = data.map(d => d.impact);
   .range([0, innerHeight])
 
   let tooltip;
+  let visible;
 </script>
 
 <h1>Extreme weather attribution study tracker</h1>
 <Legend {legendScale}></Legend>
 <div class="chart-container" bind:clientWidth={width}>
-<svg {width} {height} on:click={() => {
-  tooltip = null;
-}}>
+<svg {width} {height}>
   <g class="inner-chart" transform="translate({margin.left}, {margin.top})">
     <AxisX xScale={xScale} height={innerHeight} width={innerWidth}></AxisX>
     <AxisY {yScale}></AxisY>
-    {#if tooltip}
-    <line x1={tooltip.x} x2={tooltip.x} y1={innerHeight} y2={tooltip.y} stroke={legendScale(tooltip.impact)} stroke-width="2" out:fade/>
-    {/if}
     {#each nodes as node}
     <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
     <circle
@@ -122,7 +118,19 @@ const impact = data.map(d => d.impact);
   </g>
 </svg>
   {#if tooltip}
-    <Tooltip data={tooltip} {legendScale}></Tooltip>
+    <!-- <Tooltip data={tooltip} {legendScale}></Tooltip> -->
+    <div class="tooltip">
+      <div class="close-tooltip" on:click={() => {
+        tooltip = null;
+      }}>X</div>
+      <h3>
+          <span class="impact" style="background-color:{legendScale(data.impact)}">{data.impact}</span><a href="{data.study}" target="_blank" rel="noreferrer">{data.tooltipDate},{data.country}</a>
+      </h3>
+          <p><span class="key">Outcome:</span>{data.outcomeSummary}</p>
+          <p><span class="key">Impacts:</span>{data.impactSummary}</p>
+          <p><span class="key">Vulnerability:</span>{data.vulnerabilitySummary}</p>
+          <p><span class="key">Resilience:</span>{data.resilienceSummary}</p>
+  </div>
   {/if}
 </div>
 
@@ -143,4 +151,38 @@ const impact = data.map(d => d.impact);
   circle:focus{
     outline:none;
   }
+  .tooltip{
+        position: absolute;
+        background: rgba(255, 255, 255, 0.95);
+        box-shadow: 2px 3px 8px rgba(0,0,0,0.15);
+        padding: 2.5em;
+        border-radius: 3px;
+        display: inline-grid;
+        width: auto;
+        top:5%;
+        left:20%;
+        right:10%;
+        bottom:10%;
+    }
+    .close-tooltip{
+        position: absolute;
+        right: 0;
+        margin-right: 1.2em;
+        margin-top: 1.2em;
+        font-size: 1.2em;
+        font-weight: 700;
+    }
+    .close-tooltip:hover{
+        filter: brightness(15%);
+        cursor: pointer;
+    }
+    .impact{
+        border-radius:3px;
+        padding:1.2px;
+        width: fit-content;
+        color: #f4f4f4;
+    }
+    span.key{
+        font-weight:700;
+    }
 </style>
