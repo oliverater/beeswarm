8cb4c091c7c6855ec4b2acf28203cffae3d08aaf
oliverater <tomoliverprater@gmail.com>
2023-10-13 18:32:16 +0100

    Update AxisX.svelte

--- src/components/AxisX.svelte
+++ src/components/AxisX.svelte
@@ -7,14 +7,35 @@
     // $: console.log( xScale.ticks())
 
     $: ticks = xScale.ticks()
+
 </script>
 
 <g class="axis x">
+<<<<<<< Updated upstream
     {#each ticks as tick}
         <g class="tick" transform="translate({xScale(tick)}, 0)">
             <text x="-20" y={height+20}>{tick}</text>
             <line x1="0" x2="0" y1="0" y2={height} stroke="#d1d1d1"></line>
+=======
+     {#each ticks as tick}
+    <!-- {console.log(tick)} -->
+        <g class="tick" transform="translate({xScale(tick)}, 0)">
+            <text x="-20" y={height+20}>{tick}</text>
+            <line x1="0" x2="0" y1="0" y2={height} stroke="#d1d1d1" width="{innerWidth/9}"></line>
+            <line x1="{xScale(tick+0.25)}" x2="{xScale(tick+0.25)}" y1="0" y2={height} stroke="#efefef"></line>
+            <line class="mid" x1="{xScale(tick-0.25)}" x2="{xScale(tick-0.25)}" y1="0" y2={height} stroke="#efefef"></line>
+            <line x1="{xScale(tick+0.5)}" x2="{xScale(tick+0.5)}" y1="0" y2={height} stroke="#efefef"></line>
+            <line class="mid" x1="{xScale(tick-0.5)}" x2="{xScale(tick-0.5)}" y1="0" y2={height} stroke="#efefef"></line>
+            <line x1="{xScale(tick+0.75)}" x2="{xScale(tick+0.75)}" y1="0" y2={height} stroke="#efefef"></line>
+            <line class="mid" x1="{xScale(tick-0.75)}" x2="{xScale(tick-0.75)}" y1="0" y2={height} stroke="#efefef"></line>
+>>>>>>> Stashed changes
     </g>
     {/each}
     <text class="axis-title" x={width/2} y={height+30} text-anchor="middle" dominant-baseline="hanging" dy="5">Years</text>
 </g>
+
+<style>
+    line.mid:first-child{
+        display: none !important;
+    }
+</style>
